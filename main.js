67030701c818a84bc3d4a046dc19e7bc0688c752
cTn <ctndesigner@gmail.com>
2014-02-06 00:59:23 +0100

    adding array_difference function to the main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var debug = true;
+
 // Get access to the background window object
 // This object is used to pass current connectionId to the backround page
 // so the onClosed event can close the port for us if it was left opened, without this
@@ -8,4 +10,23 @@ chrome.runtime.getBackgroundPage(function(result) {
 });
 
 $(document).ready(function() {
+    PortHandler.initialize();
 });
+
+// accepting single level array with "value" as key
+function array_difference(firstArray, secondArray) {
+    var cloneArray = [];
+    
+    // create hardcopy
+    for (var i = 0; i < firstArray.length; i++) {
+        cloneArray.push(firstArray[i]);
+    }
+    
+    for (var i = 0; i < secondArray.length; i++) {
+        if (cloneArray.indexOf(secondArray[i]) != -1) {
+            cloneArray.splice(cloneArray.indexOf(secondArray[i]), 1);
+        }
+    }
+    
+    return cloneArray;
+}
