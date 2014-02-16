c0e08c89bda4fd970f9d01cb8eb81cd2bf95af60
cTn <ctndesigner@gmail.com>
2014-02-17 00:29:01 +0100

    lower window size, linux window size bugfix

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    start_app();
-});
-
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == 'update') {
-    }
-});
-
-chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
-});
-
+/*
+    resizable: false - Keep in mind this only disables the side/corner resizing via mouse, nothing more
+    maxWidth / maxHeight - is defined to prevent application reaching maximized state through window manager
+    
+    We are setting Bounds through setBounds method after window was created because on linux setting Bounds as
+    window.create property seemed to fail, probably because "previous" bounds was used instead according to docs.
+    
+    bounds - Size and position of the content in the window (excluding the titlebar). 
+    If an id is also specified and a window with a matching id has been shown before, the remembered bounds of the window will be used instead.
+*/
 function start_app() {
     chrome.app.window.create('main.html', {
         id: 'main-window',
-        frame: 'native',
+        frame: 'chrome',
         resizable: false,
-        minWidth: 960,
-        minHeight: 357,
-        maxWidth: 960,
+        maxWidth: 600,
         maxHeight: 357
     }, function(main_window) {
+        // set window size
+        main_window.setBounds({'width': 600, 'height': 357});    
+    
+        // bind events
         main_window.onClosed.addListener(function() {
             // connectionId is passed from the script side through the chrome.runtime.getBackgroundPage refference
             // allowing us to automatically close the port when application shut down
@@ -37,3 +37,15 @@ function start_app() {
         });
     });
 }
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    start_app();
+});
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason == 'update') {
+    }
+});
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
+});
