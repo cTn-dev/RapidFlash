2bb81ef3a8bd285ce614e2f67d8c3c5deec8797c
cTn <ctndesigner@gmail.com>
2014-02-17 02:38:47 +0100

    working hex save procedure (test string inside)

--- main.js
+++ main.js
@@ -73,6 +73,49 @@ $(document).ready(function() {
     });
     
     $('a.save').click(function() {
+        // we could parse the name as variable
+        chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: 'firmware_test', accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+            if (!fileEntry) {
+                // no "valid" file selected/created, aborting
+                console.log('No valid file selected, aborting');
+                return;
+            }
+            
+            chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+                console.log('Saving firmware to: ' + path);
+                GUI.log('Saving firmware to: <strong>' + path + '</strong>');
+                
+                // change file entry from read only to read/write
+                chrome.fileSystem.getWritableEntry(fileEntry, function(fileEntryWritable) {
+                    // check if file is writable
+                    chrome.fileSystem.isWritableEntry(fileEntryWritable, function(isWritable) {
+                        if (isWritable) {
+                            var hex = "Real hex content goes here";
+                            
+                            var blob = new Blob([hex], {type: 'text/plain'}); // first parameter for Blob needs to be an array
+                            
+                            fileEntryWritable.createWriter(function(writer) {
+                                writer.onerror = function (e) {
+                                    console.error(e);
+                                };
+                                
+                                writer.onwriteend = function() {
+                                    // all went fine
+                                    callback(true);
+                                };
+                                
+                                writer.write(blob);
+                            }, function (e) {
+                                console.error(e);
+                            });
+                        } else {
+                            // Something went wrong or file is set to read only and cannot be changed
+                            console.log('You don\'t have write permissions for this file, sorry.');
+                        }
+                    });
+                });
+            });
+        });
     });
     
     $('a.flash').click(function() {
