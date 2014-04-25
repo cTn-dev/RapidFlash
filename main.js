8cff199a361119f31ff5e87b4f61be3f54182f85
cTn <ctndesigner@gmail.com>
2014-04-25 18:18:03 +0200

    little bit of validation on save hex button

--- main.js
+++ main.js
@@ -96,61 +96,62 @@ $(document).ready(function() {
         });
     });
 
-    // TODO file will only contain "undefined" string if hex parser wasn't call before (ihex.raw will be undefined)
-    // we should probably lock the save button while ihex.raw isn't ready
     $('a.save').click(function() {
-        // TODO some sort of validation
-        var name = $('select#firmware').val();
+        if (ihex.raw != undefined && ihex.raw != 'undefined') {
+            var name = $('select#firmware').val();
 
-        chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: name, accepts: [{extensions: ['hex']}]}, function(fileEntry) {
-            if (!fileEntry) {
-                // no "valid" file selected/created, aborting
-                console.log('No valid file selected, aborting');
-                return;
-            }
+            chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: name, accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+                if (!fileEntry) {
+                    // no "valid" file selected/created, aborting
+                    console.log('No valid file selected, aborting');
+                    return;
+                }
 
-            chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
-                console.log('Saving firmware to: ' + path);
-                GUI.log('Saving firmware to: <strong>' + path + '</strong>');
+                chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+                    console.log('Saving firmware to: ' + path);
+                    GUI.log('Saving firmware to: <strong>' + path + '</strong>');
 
-                // change file entry from read only to read/write
-                chrome.fileSystem.getWritableEntry(fileEntry, function(fileEntryWritable) {
-                    // check if file is writable
-                    chrome.fileSystem.isWritableEntry(fileEntryWritable, function(isWritable) {
-                        if (isWritable) {
-                            var blob = new Blob([ihex.raw], {type: 'text/plain'}); // first parameter for Blob needs to be an array
+                    // change file entry from read only to read/write
+                    chrome.fileSystem.getWritableEntry(fileEntry, function(fileEntryWritable) {
+                        // check if file is writable
+                        chrome.fileSystem.isWritableEntry(fileEntryWritable, function(isWritable) {
+                            if (isWritable) {
+                                var blob = new Blob([ihex.raw], {type: 'text/plain'}); // first parameter for Blob needs to be an array
 
-                            fileEntryWritable.createWriter(function(writer) {
-                                writer.onerror = function (e) {
+                                fileEntryWritable.createWriter(function(writer) {
+                                    writer.onerror = function (e) {
+                                        console.error(e);
+                                    };
+
+                                    var truncated = false;
+                                    writer.onwriteend = function() {
+                                        if (!truncated) {
+                                            // onwriteend will be fired again when truncation is finished
+                                            truncated = true;
+                                            writer.truncate(blob.size);
+
+                                            return;
+                                        }
+
+                                        // all went fine
+                                        callback(true);
+                                    };
+
+                                    writer.write(blob);
+                                }, function (e) {
                                     console.error(e);
-                                };
-
-                                var truncated = false;
-                                writer.onwriteend = function() {
-                                    if (!truncated) {
-                                        // onwriteend will be fired again when truncation is finished
-                                        truncated = true;
-                                        writer.truncate(blob.size);
-
-                                        return;
-                                    }
-
-                                    // all went fine
-                                    callback(true);
-                                };
-
-                                writer.write(blob);
-                            }, function (e) {
-                                console.error(e);
-                            });
-                        } else {
-                            // Something went wrong or file is set to read only and cannot be changed
-                            console.log('You don\'t have write permissions for this file, sorry.');
-                        }
+                                });
+                            } else {
+                                // Something went wrong or file is set to read only and cannot be changed
+                                console.log('You don\'t have write permissions for this file, sorry.');
+                            }
+                        });
                     });
                 });
             });
-        });
+        } else {
+            GUI.log('You need to <strong>load</strong> a valid firmware before you can save it');
+        }
     });
 
     $('a.flash').click(function() {
