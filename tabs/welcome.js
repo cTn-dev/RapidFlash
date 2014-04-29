1dbf9e6058ce99062490160ee9836e0a998632f5
cTn <ctndesigner@gmail.com>
2014-04-29 18:53:40 +0200

    polished welcome tab + added changelog

--- tabs/welcome.js
+++ tabs/welcome.js
@@ -1,6 +1,6 @@
 function tab_initialize_welcome() {
     $('#content').load("./tabs/welcome.html", function() {
-        // move donation box inside content area
-        // load custom firmware and flash buttons should reside outside of content area
+        // load changelog content
+        $('div.changelog .wrapper').load('./changelogs/app.html');
     });
 }
