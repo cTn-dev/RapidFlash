f82d4078873f915dae7437402158102acad57369
cTn <ctndesigner@gmail.com>
2014-06-16 22:02:06 +0200

    sort out canary errors

--- js/port_handler.js
+++ js/port_handler.js
@@ -109,7 +109,13 @@ port_handler.prototype.check = function() {
             self.initial_ports = current_ports;
         }
 
-        check_usb_devices();
+        if (GUI.optional_usb_permissions) {
+            check_usb_devices();
+        }
+
+        self.main_timeout_reference = setTimeout(function() {
+            self.check();
+        }, 250);
     });
 
     function check_usb_devices() {
@@ -125,10 +131,6 @@ port_handler.prototype.check = function() {
                 }
             }
         });
-
-        self.main_timeout_reference = setTimeout(function() {
-            self.check();
-        }, 250);
     }
 };
 
