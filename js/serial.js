18d24e49a9f2c351d9050e5d585a22197ea7f0c4
cTn <ctndesigner@gmail.com>
2014-06-30 02:37:24 +0200

    serial error events hookup

--- js/serial.js
+++ js/serial.js
@@ -21,6 +21,7 @@ var serial = {
 
                 self.onReceiveError.addListener(function watch_for_on_receive_errors(info) {
                     console.error(info);
+                    ga_tracker.sendEvent('Error', 'Serial', info.error);
 
                     // valid conditions are 'disconnected', 'timeout', 'device_lost', 'system_error'
                     if (info.error == 'system_error') {
@@ -36,6 +37,7 @@ var serial = {
                 callback(connectionInfo);
             } else {
                 console.log('SERIAL: Failed to open serial port');
+                ga_tracker.sendEvent('Error', 'Serial', 'FailedToOpen');
                 callback(false);
             }
         });
@@ -59,6 +61,7 @@ var serial = {
                 console.log('SERIAL: Connection with ID: ' + self.connectionId + ' closed');
             } else {
                 console.log('SERIAL: Failed to close connection with ID: ' + self.connectionId + ' closed');
+                ga_tracker.sendEvent('Error', 'Serial', 'FailedToClose');
             }
 
             console.log('SERIAL: Statistics - Sent: ' + self.bytes_sent + ' bytes, Received: ' + self.bytes_received + ' bytes');
