392241428025264b8a07302b2c64a83fc052d896
cTn <ctndesigner@gmail.com>
2014-07-17 01:15:35 +0200

    fix bounds for windows 7 (read note)

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
 /*
-    resizable: false - Keep in mind this only disables the side/corner resizing via mouse, nothing more
-    maxWidth / maxHeight - is defined to prevent application reaching maximized state through window manager
-
-    We are setting Bounds through setBounds method after window was created because on linux setting Bounds as
-    window.create property seemed to fail, probably because "previous" bounds was used instead according to docs.
-
-    bounds - Size and position of the content in the window (excluding the titlebar).
     If an id is also specified and a window with a matching id has been shown before, the remembered bounds of the window will be used instead.
+
+    Size calculation for innerBounds seems to be faulty, app was designed for 740x420
+
+    Bug was confirmed on Windows 7
+    OSX seems to be unaffected
+    Linux and cros is unknown
+
+    I am using arbitrary dimensions which fixes the Windows 7 problem, hopefully it will get resolved in future release so other OSs won't have to
+    use bigger dimensions by default.
 */
 function start_app() {
     chrome.app.window.create('main.html', {
         id: 'main-window',
         frame: 'chrome',
-        minWidth: 700,
-        minHeight: 420
+        innerBounds: {
+            minWidth: 714,
+            minHeight: 427
+        }
     }, function(main_window) {
         main_window.onClosed.addListener(function() {
             // connectionId is passed from the script side through the chrome.runtime.getBackgroundPage refference
