3318c8aba86bfb8f6aa3340f56f402cea0f25f56
cTn <ctndesigner@gmail.com>
2014-08-10 05:20:48 +0200

    cleaning up

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // Get access to the background window object
 // This object is used to pass variables between active page and background page
 var backgroundPage;
-chrome.runtime.getBackgroundPage(function(result) {
+chrome.runtime.getBackgroundPage(function (result) {
     backgroundPage = result;
     backgroundPage.app_window = window;
 });
@@ -12,11 +12,11 @@ chrome.runtime.getBackgroundPage(function(result) {
 var googleAnalyticsService = analytics.getService('ice_cream_app');
 var googleAnalytics = googleAnalyticsService.getTracker('UA-32728876-8');
 var googleAnalyticsConfig = false;
-googleAnalyticsService.getConfig().addCallback(function(config) {
+googleAnalyticsService.getConfig().addCallback(function (config) {
     googleAnalyticsConfig = config;
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     googleAnalytics.sendAppView('Application Started');
 
     PortHandler.initialize();
@@ -35,12 +35,12 @@ $(document).ready(function() {
 
     // generate list of releases
     var e_releases = $('select#release');
-    request_releases(function(releases) {
+    request_releases(function (releases) {
         for (var i = 0; i < releases.length; i++) {
             e_releases.append('<option value="' + releases[i] + '">' + releases[i] + '</option>');
         }
 
-        chrome.storage.local.get('release', function(result) {
+        chrome.storage.local.get('release', function (result) {
             if (result.release) {
                 // check if release is whitelisted (exists), if yes select
                 var options = $('select#release option');
@@ -56,36 +56,36 @@ $(document).ready(function() {
     });
 
     // auto-select certain settings if they were saved
-    chrome.storage.local.get('programmer', function(result) {
+    chrome.storage.local.get('programmer', function (result) {
         if (result.programmer) $('select#programmer').val(result.programmer);
     });
 
-    chrome.storage.local.get('firmware', function(result) {
+    chrome.storage.local.get('firmware', function (result) {
         if (result.firmware) $('select#firmware').val(result.firmware);
     });
 
     // UI hooks
     // app options
-    $('a#app_options').click(function() {
+    $('a#app_options').click(function () {
         var el = $(this);
 
         if (!el.hasClass('active')) {
             el.addClass('active');
             el.after('<div id="app_options-window"></div>');
-            $('div#app_options-window').load('./tabs/app_options.html', function() {
+            $('div#app_options-window').load('./tabs/app_options.html', function () {
                 googleAnalytics.sendAppView('Options');
 
                 // translate to user-selected language
                 localize();
 
                 // if notifications are enabled, or wasn't set, check the notifications checkbox
-                chrome.storage.local.get('update_notify', function(result) {
+                chrome.storage.local.get('update_notify', function (result) {
                     if (typeof result.update_notify === 'undefined' || result.update_notify) {
                         $('div.notifications input').prop('checked', true);
                     }
                 });
 
-                $('div.notifications input').change(function() {
+                $('div.notifications input').change(function () {
                     var check = $(this).is(':checked');
 
                     chrome.storage.local.set({'update_notify': check});
@@ -96,7 +96,7 @@ $(document).ready(function() {
                     $('div.statistics input').prop('checked', true);
                 }
 
-                $('div.statistics input').change(function() {
+                $('div.statistics input').change(function () {
                     var result = $(this).is(':checked');
                     googleAnalyticsConfig.setTrackingPermitted(result);
                 });
@@ -105,7 +105,7 @@ $(document).ready(function() {
                     if (e.type == 'click' && !$.contains($('div#app_options-window')[0], e.target) || e.type == 'keyup' && e.keyCode == 27) {
                         $(document).unbind('click keyup', close_and_cleanup);
 
-                        $('div#app_options-window').slideUp(function() {
+                        $('div#app_options-window').slideUp(function () {
                             el.removeClass('active');
                             $(this).empty().remove();
                         });
@@ -121,10 +121,10 @@ $(document).ready(function() {
 
     // Tabs
     var tabs = $('#tabs > ul');
-    $('a', tabs).click(function() {
+    $('a', tabs).click(function () {
         if ($(this).parent().hasClass('active') == false) { // only initialize when the tab isn't already active
-            var self = this;
-            var index = $(self).parent().index();
+            var self = this,
+                index = $(self).parent().index();
 
             // disable previously active tab highlight
             $('li', tabs).removeClass('active');
@@ -157,8 +157,8 @@ $(document).ready(function() {
 
     $('#tabs a:first').click();
 
-    $('a.load').click(function() {
-        chrome.fileSystem.chooseEntry({type: 'openFile', accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+    $('a.load').click(function () {
+        chrome.fileSystem.chooseEntry({type: 'openFile', accepts: [{extensions: ['hex']}]}, function (fileEntry) {
             if (!fileEntry) {
                 // no "valid" file selected/created, aborting
                 console.log('No valid file selected, aborting');
@@ -169,13 +169,13 @@ $(document).ready(function() {
             ihex.raw = false;
             ihex.parsed = false;
 
-            chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+            chrome.fileSystem.getDisplayPath(fileEntry, function (path) {
                 console.log('Loading file from: ' + path);
 
-                fileEntry.file(function(file) {
+                fileEntry.file(function (file) {
                     var reader = new FileReader();
 
-                    reader.onprogress = function(e) {
+                    reader.onprogress = function (e) {
                         if (e.total > 1048576) { // 1 MB
                             // dont allow reading files bigger then 1 MB
                             console.log('File limit (1 MB) exceeded, aborting');
@@ -184,7 +184,7 @@ $(document).ready(function() {
                         }
                     };
 
-                    reader.onloadend = function(e) {
+                    reader.onloadend = function (e) {
                         if (e.total != 0 && e.total == e.loaded) {
                             console.log('File loaded');
 
@@ -192,9 +192,9 @@ $(document).ready(function() {
                             var worker = new Worker('./js/workers/hex_parser.js');
 
                             // "callback"
-                            worker.onmessage = function (event) {
-                                if (event.data) {
-                                    ihex.parsed = event.data;
+                            worker.onmessage = function (ev) {
+                                if (ev.data) {
+                                    ihex.parsed = ev.data;
 
                                     GUI.log('Custom HEX file <span style="color: green">loaded</span>');
                                     $('select#firmware').val('custom').change();
@@ -217,33 +217,33 @@ $(document).ready(function() {
         });
     });
 
-    $('a.save').click(function() {
-        var name = $('select#firmware').val();
-        var release = $('select#release').val();
+    $('a.save').click(function () {
+        var name = $('select#firmware').val(),
+            release = $('select#release').val();
 
         if (name != '0' && name != 'custom') {
             if (release != '0') {
                 request_firmware(save);
 
                 var save = function () {
-                    chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: name, accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+                    chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: name, accepts: [{extensions: ['hex']}]}, function (fileEntry) {
                         if (!fileEntry) {
                             console.log('No valid file selected, aborting');
                             return;
                         }
 
-                        chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+                        chrome.fileSystem.getDisplayPath(fileEntry, function (path) {
                             console.log('Saving firmware to: ' + path);
                             GUI.log('Saving firmware to: <strong>' + path + '</strong>');
 
                             // change file entry from read only to read/write
-                            chrome.fileSystem.getWritableEntry(fileEntry, function(fileEntryWritable) {
+                            chrome.fileSystem.getWritableEntry(fileEntry, function (fileEntryWritable) {
                                 // check if file is writable
-                                chrome.fileSystem.isWritableEntry(fileEntryWritable, function(isWritable) {
+                                chrome.fileSystem.isWritableEntry(fileEntryWritable, function (isWritable) {
                                     if (isWritable) {
                                         var blob = new Blob([ihex.raw], {type: 'text/plain'}); // first parameter for Blob needs to be an array
 
-                                        fileEntryWritable.createWriter(function(writer) {
+                                        fileEntryWritable.createWriter(function (writer) {
                                             writer.onerror = function (e) {
                                                 console.error(e);
                                             };
@@ -283,7 +283,7 @@ $(document).ready(function() {
         }
     });
 
-    $('a.reset').click(function() {
+    $('a.reset').click(function () {
         properties = [];
 
         switch (GUI.active_tab) {
@@ -298,7 +298,7 @@ $(document).ready(function() {
 
     // backend
     function begin_upload(hex) {
-        switch($('select#programmer').val()) {
+        switch ($('select#programmer').val()) {
             case 'turnigy_usb_linker':
                 STK500V2.connect(9600, hex);
                 break;
@@ -318,7 +318,7 @@ $(document).ready(function() {
         googleAnalytics.sendEvent('Flashing', 'Firmware', $('select#firmware').val());
     }
 
-    $('a.flash').click(function() {
+    $('a.flash').click(function () {
         if (!GUI.connect_lock) {
             if ($('select#programmer').val() != '0') {
                 if ($('select#firmware').val() != '0') {
@@ -332,7 +332,7 @@ $(document).ready(function() {
                                 chrome.storage.local.set({'release': $('select#release').val()});
 
                                 // Request firmware
-                                request_firmware(function(raw, parsed) {
+                                request_firmware(function (raw, parsed) {
                                     begin_upload(parsed);
                                 });
                             } else {
@@ -363,7 +363,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#content").on('keydown', 'input[type="number"]', function(e) {
+    $("#content").on('keydown', 'input[type="number"]', function (ev) {
         // whitelist all that we need for numeric control
         var whitelist = [
             96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, // numpad and standard number keypad
@@ -373,15 +373,16 @@ $(document).ready(function() {
             37, 38, 39, 40, 13 // arrows and enter
         ];
 
-        if (whitelist.indexOf(e.keyCode) == -1) e.preventDefault();
+        if (whitelist.indexOf(ev.keyCode) == -1) ev.preventDefault();
     });
 
-    $("#content").on('change', 'input[type="number"]', function() {
-        var element = $(this);
-        var min = parseFloat(element.prop('min'));
-        var max = parseFloat(element.prop('max'));
-        var step = parseFloat(element.prop('step'));
-        var val = parseFloat(element.val());
+    $("#content").on('change', 'input[type="number"]', function () {
+        var element = $(this),
+            min = parseFloat(element.prop('min')),
+            max = parseFloat(element.prop('max')),
+            step = parseFloat(element.prop('step')),
+            val = parseFloat(element.val()),
+            decimal_places;
 
         // only adjust minimal end if bound is set
         if (element.prop('min')) {
@@ -407,7 +408,7 @@ $(document).ready(function() {
 
         // if step is set and is float and value is int, convert to float, keep decimal places in float according to step *experimental*
         if (!isNaN(step) && step % 1 !== 0) {
-            var decimal_places = String(step).split('.')[1].length;
+            decimal_places = String(step).split('.')[1].length;
 
             if (val % 1 === 0) {
                 element.val(val.toFixed(decimal_places));
